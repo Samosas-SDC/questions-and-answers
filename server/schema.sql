880b0746f0736ec074659883f8044b7afceec5da
James <qjameso@berkeley.edu>
2022-04-22 16:18:05 -0700

    refactor: edit data types and add notes

--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,19 @@
 --psql -h localhost -U postgres -d qadb -f server/schema.sql
 
---ETL
--- COPY question (id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness) FROM ‘/Users/james/questions.csv’ DELIMITER ‘,’ CSV HEADER;
+-- *****ETL*****
+-- COPY question (id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness) FROM '/Users/james/questions.csv' DELIMITER ',' CSV HEADER;
+-- COPY answer (id, question_id, body, answer_date, answerer_name, answerer_email, reported, helpfulness) FROM '/Users/james/answers.csv' DELIMITER ',' CSV HEADER;
+-- COPY answerPhoto (id, answer_id, url) FROM '/Users/james/answers_photos.csv' DELIMITER ',' CSV HEADER;
 
--- COPY answer (id, question_id, body, answer_date, answerer_name, answerer_email, reported, helpfulness) FROM ‘/Users/james/answers.csv’ DELIMITER ‘,’ CSV HEADER;
 
--- COPY answerPhoto (id, answer_id, url) FROM ‘/Users/james/answers_photos.csv’ DELIMITER ‘,’ CSV HEADER;
+--AFTER IMPORTING DATA FROM CSV, RUN THE FOLLOWING TO REFORMAT DATA:
+--ALTER TABLE question ALTER COLUMN question_date SET DATA TYPE timestamp with time zone USING to_timestamp(question_date/1000);
+--ALTER TABLE question ALTER COLUMN reported SET DATA TYPE boolean USING reported::boolean;
+--ALTER TABLE question ALTER COLUMN reported SET DEFAULT false;
+
+--ALTER TABLE answer ALTER COLUMN answer_date SET DATA TYPE timestamp with time zone USING to_timestamp(answer_date/1000);
+--ALTER TABLE answer ALTER COLUMN reported SET DATA TYPE boolean USING reported::boolean;
+--ALTER TABLE answer ALTER COLUMN reported SET DEFAULT false;
 
 DROP TABLE IF EXISTS question cascade;
 DROP TABLE IF EXISTS answer cascade;
@@ -19,7 +27,7 @@ CREATE TABLE question (
   asker_name VARCHAR(60) NOT NULL,
   asker_email VARCHAR(60) NOT NULL,
   question_helpfulness INT NOT NULL,
-  reported INT NOT NULL DEFAULT 0,
+  reported INT NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -30,7 +38,7 @@ CREATE TABLE answer (
   answer_date BIGINT NOT NULL,
   answerer_name VARCHAR(60) NOT NULL,
   answerer_email VARCHAR(60) NOT NULL,
-  reported INT NOT NULL DEFAULT 0,
+  reported INT NOT NULL,
   helpfulness INT,
   PRIMARY KEY (id),
   CONSTRAINT fk_question
@@ -51,3 +59,4 @@ CREATE TABLE answerPhoto (
 CREATE INDEX product_id ON question (product_id);
 CREATE INDEX question_id ON answer (question_id);
 CREATE INDEX answer_id ON answerPhoto (answer_id);
+
