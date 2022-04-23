ce55ef36cb00d4bfc4d81a83eea6b394d2c0af86
James <qjameso@berkeley.edu>
2022-04-23 14:06:06 -0700

    refactor: structure response obj via queries instead of JS

--- server/index.js
+++ server/index.js
@@ -5,13 +5,12 @@ const models = require('./models/models');
 
 let PORT = process.env.SERVER_PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
 
 app.get('/qa/questions', (req, res) => {
   let formattedResponse = {};
-  // let questionId = req.params.question_id;
   let productId = req.query.product_id;
   let page = req.query.page || 1;
   let count = req.query.count || 5;
@@ -25,6 +24,24 @@ app.get('/qa/questions', (req, res) => {
       formattedResponse = {
         product_id: productId,
         results: response.rows
+      }
+
+      return Promise.all(response.rows.map(question => models.getAnswers({
+        question_id: question.id,
+        count: count
+      })))
+    })
+    .then(answers => {
+      for (let i = 0; i < answers.length; i++) {
+        answers[i].rows.forEach(ans => {
+          if (!formattedResponse.results[i].answers) {
+            formattedResponse.results[i].answers = {
+              [ans.id]: ans
+            }
+          } else {
+            formattedResponse.results[i].answers[ans.id] = ans;
+          }
+        });
       }
 
       res.status(200).send(formattedResponse);
@@ -53,12 +70,6 @@ app.get(`/qa/questions/:question_id/answers`, (req, res) => {
         results: response.rows
       };
 
-      return Promise.all(response.rows.map(answer => models.getPhotos(answer.id)));
-    })
-    .then(photos => {
-      for (let i = 0; i < photos.length; i++) {
-        formattedResponse.results[i].photos = photos[i].rows;
-      }
       res.status(200).send(formattedResponse);
     })
     .catch(err => {
--- server/models/models.js
+++ server/models/models.js
@@ -2,20 +2,21 @@ let db = require('../db');
 
 module.exports = {
   getQuestions: function (params) {
-    let text = 'SELECT id, question_body, question_date, asker_name, question_helpfulness, reported FROM question WHERE product_id=$1 LIMIT $2;';
+    let text = `SELECT id, question_body, question_date, asker_name, question_helpfulness, reported
+      FROM question
+      WHERE product_id=$1
+      LIMIT $2;`;
     let values = [params.product_id, params.count];
 
     return db.query(text, values);
   },
   getAnswers: function (params) {
-    let text = 'SELECT id, body, answer_date, answerer_name, helpfulness FROM answer WHERE question_id=$1 LIMIT $2;';
+    let text = `SELECT answer.id, answer.body, answer.answer_date, answer.answerer_name, answer.helpfulness, array_agg(json_build_object('id', (answerPhoto.id), 'url', (answerPhoto.url))) AS photos
+      FROM answer JOIN answerPhoto ON answer.id=answerPhoto.answer_id
+      WHERE question_id=$1
+      GROUP BY answer.id
+      LIMIT $2;`;
     let values = [params.question_id, params.count];
-
-    return db.query(text, values);
-  },
-  getPhotos: function (answerId) {
-    let text = 'SELECT id, url FROM answerPhoto WHERE answer_id=$1';
-    let values = [answerId];
 
     return db.query(text, values);
   },
