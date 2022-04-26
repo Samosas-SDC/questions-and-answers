e13ec4a0049b74da18ba9ea0ff78db2b81e72e2a
James <qjameso@berkeley.edu>
2022-04-26 12:15:07 -0700

    test: add more options and refactor test

--- server/spec/answer.test.js
+++ server/spec/answer.test.js
@@ -1,16 +1,27 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
 
 export const options = {
-  vus: 100,
-  duration: '30s',
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1000,
+      timeUnit: '1s', //1000 RPS
+      duration: '30s',
+      preAllocatedVUs: 100, //inital pool of VUs
+      maxVUs: 750,
+    },
+  },
 };
 
 export default function () {
   //request data from the bottom of the dataset
-  for (let id = 3518948; id <= 3518963; id++) {
-    http.get(`http://localhost:3000/qa/questions/${id}/answers`, {
-      tags: { name: 'GetAnswersURL' }
-    });
-  }
-}
+  // for (let id = 3518948; id <= 3518963; id++) {
+  //   http.get(`http://localhost:3000/qa/questions/${id}/answers`, {
+  //     tags: { name: 'GetAnswersURL' }
+  //   });
+  // }
+
+  http.get(`http://localhost:3000/qa/questions/${Math.floor(Math.random() * 3518963)}/answers`);
+};
--- server/spec/question.test.js
+++ server/spec/question.test.js
@@ -1,19 +1,27 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
 
 export const options = {
-  vus: 100,
-  duration: '30s',
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1000,
+      timeUnit: '1s', //1000 RPS
+      duration: '30s',
+      preAllocatedVUs: 100, //inital pool of VUs
+      maxVUs: 1000,
+    },
+  },
 };
 
 export default function () {
-  // http.get('http://localhost:3000/qa/questions?product_id=1');
-  // sleep(1);
-
   //request data from the bottom of the dataset
-  for (let id = 1000000; id <= 1000011; id++) {
-    http.get(`http://localhost:3000/qa/questions?product_id=${id}`, {
-      tags: { name: 'GetQuestionsURL' }
-    });
-  }
-}
+  // for (let id = 1000000; id <= 1000011; id++) {
+  //   http.get(`http://localhost:3000/qa/questions?product_id=${id}`, {
+  //     tags: { name: 'GetQuestionsURL' }
+  //   });
+  // }
+
+  http.get(`http://localhost:3000/qa/questions?product_id=${Math.floor(Math.random() * 1000000)}`);
+};
